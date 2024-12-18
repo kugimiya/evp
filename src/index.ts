a529cc3365df7c3084177788e6e14a86bd946fcd
kugimiya <bp.001@yandex.ru>
2024-12-18 14:38:39 +0400

    chore: typo

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import { createFullSubtractor } from "./prefabs/FullSubstractor";
 const solver = new Solver();
 const { rootWire, operandAIO, operandBIO, operandCIO } = createFullSubtractor(
   solver,
-  64,
+  32,
 );
 
 const time = Date.now();
 solver.step(rootWire.id);
 console.log(`MAIN: step time = ${Date.now() - time}ms`);
+console.log(
+  `MAIN: simulated objects = ${Object.keys(solver.linker.objects).length}, with links count = ${solver.linker.links.length}`,
+);
 console.log(
   "[in]  A =",
   operandAIO.map((object) => object.outPorts[0][2]).join(""),
